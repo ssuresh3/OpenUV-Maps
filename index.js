8e8d3623c40584935c03df80fb636b959015c33e
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2020-01-18 16:27:00 -0800

    update server

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, "0.0.0.0", () => console.log(`Example app listening on port ${port}!`))
